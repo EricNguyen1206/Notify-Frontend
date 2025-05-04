8de48c9ddbf9aa949aa607b682cff240a63f4b7c
tinnt1 <tinnt1@runsystem.net>
2025-05-04 16:23:44 +0700

    feat: update config frontend

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "github.com",
+        },
+        {
+          protocol: "https",
+          hostname: process.env.NEXT_PUBLIC_SUPABASE_HOST,
+        },
+        {
+          protocol: "https",
+          hostname: "avatars.githubusercontent.com",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
